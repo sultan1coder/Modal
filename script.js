d1feb25e73ad0aa74b969e7209fdd1a3b538405a
ashraf <sadarsign@gmail.com>
2025-03-18 17:31:56 +0300

    Initialized

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+console.log(btnsOpenModal);
+
+/*
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -28,3 +36,4 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+*/
