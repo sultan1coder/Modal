b175e7f4b43bd14b4028fa2051d4234f4eb5c8ce
ashraf <sadarsign@gmail.com>
2025-03-18 17:35:32 +0300

    Initialized

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+for(let i = 0; i < btnsOpenModal.length; i++)
+console.log(btnsOpenModal[i].textContent)
+
 /*
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
