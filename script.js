0750b11120f7fef97a1eaf99343ac4e70e6969b9
ashraf <sadarsign@gmail.com>
2025-03-18 17:42:16 +0300

    Initialized

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
-for(let i = 0; i < btnsOpenModal.length; i++)
-console.log(btnsOpenModal[i].textContent)
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('button clicked');
+  });
 
 /*
 const modal = document.querySelector('.modal');
