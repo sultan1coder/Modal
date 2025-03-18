9b79cf02e514b7c1e407507254189984944aaff6
ashraf <sadarsign@gmail.com>
2025-03-18 17:50:46 +0300

    Initialized

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ console.log(btnsOpenModal);
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
     console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
 
 /*
 const modal = document.querySelector('.modal');
