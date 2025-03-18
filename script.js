7c6bdd5258adc820fc52749c238e53173ba1884c
ashraf <sadarsign@gmail.com>
2025-03-18 18:08:13 +0300

    Initialized

--- script.js
+++ script.js
@@ -23,6 +23,13 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  console.log('A key pressed');
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
 /*
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
